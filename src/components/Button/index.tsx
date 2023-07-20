97fd61419353a1280d9c59e7aa2e20322ac8393f
Felipe Avila <f.avila@hotmail.com.br>
2023-07-20 16:23:08 -0300

    inicio da implementacao da autenticacao Google

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,14 @@
+import { TouchableOpacityProps } from "react-native";
 import { Container, ButtonText } from "./styles";
 
-type Props = {
+type Props = TouchableOpacityProps & {
     text: string,
     textColor?: string,
     bgColor?: string
 }
-export function Button({ text, bgColor = '#76364D', textColor = '#ffffff' }: Props) {
+export function Button({ text, bgColor = '#76364D', textColor = '#ffffff', ...rest }: Props) {
     return (
-        <Container bgColor={bgColor}>
+        <Container bgColor={bgColor} {...rest}>
             <ButtonText textColor={textColor}>
                 {text}
             </ButtonText>
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,8 @@
+import { useEffect } from 'react';
+import { ANDROID_CLIENT_ID } from '@env';
 import { useTheme } from 'styled-components';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 
 import BackgroundPNG from '../../assets/images/bg.png';
 import LogoPng from '../../assets/images/logo.png';
@@ -8,8 +12,29 @@ import { Button } from '../../components/Button';
 
 import { Container, Logo, Text, TextButton } from "./styles";
 
+WebBrowser.maybeCompleteAuthSession();
+
 export function Login() {
     const { COLORS } = useTheme();
+
+
+    const [_request, response, googleSignIn] = Google.useAuthRequest({
+        androidClientId: ANDROID_CLIENT_ID,
+        scopes: ['profile', 'email']
+    });
+
+    function handleGoogleSign() {
+        googleSignIn()
+            .then((response) => {
+                console.log('Resposta do login -> ', response);
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        console.log('Response -->', response);
+    }, [response]);
+
     return (
         <Container source={BackgroundPNG}>
             <Logo
@@ -37,6 +62,7 @@ export function Login() {
                 text='Entrar com Google'
                 bgColor='#ffffff'
                 textColor={COLORS.gray}
+                onPress={handleGoogleSign}
             />
 
             <TextButton>
